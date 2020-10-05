11bae7081e97d8aec9b1d9af5b60e0647d52e929
Sagarjagade <69387154+Sagarjagade@users.noreply.github.com>
2020-10-05 20:05:31 +0530

    Update Navigation.jsx

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -6,15 +6,7 @@ function Navigation()
     
     return(
         <>  
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-    crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-    crossorigin="anonymous"></script>
+           
             
        <div classNameName="container-fluid ">
           <div classNameName="row">  
@@ -31,19 +23,19 @@ function Navigation()
                             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                <NavLink exact className="nav-link "  to="/">Home</NavLink>
+                                <Link exact className="nav-link "  to="/">Home</Link>
                                 </li>
 
                                 <li className="nav-item">
-                                <NavLink exact className="nav-link "  to="/about">About</NavLink>
+                                <Link exact className="nav-link "  to="/about">About</Link>
                                 </li>
 
                                 <li className="nav-item">
-                                <NavLink exact className="nav-link "  to="/service">Service</NavLink>
+                                <Link exact className="nav-link "  to="/service">Service</Link>
                                 </li>
 
                                 <li className="nav-item">
-                                <NavLink exact className="nav-link " to="/contact">Contact</NavLink>
+                                <Link exact className="nav-link " to="/contact">Contact</Link>
                                 </li>
                     </ul>
                   
